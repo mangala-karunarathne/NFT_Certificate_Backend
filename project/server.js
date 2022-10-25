0ea70d97377535df3f30ad0811a637e01691f42c
mangala-karunarathne <mangalaicc@gmail.com>
2022-10-24 17:38:50 -0700

    FIX | cors issue

--- project/server.js
+++ project/server.js
@@ -21,15 +21,26 @@
 
 const path = require('path');
 const express = require('express');
+const cors = require('cors')
 // const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
+// const corsOptions ={
+//   origin:'http://localhost:5000', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200
+// }
+
+
+
+
 connectDB();
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -52,5 +63,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
